1e5c3c498fd1fe717d5320b65135aef11ada0c86
Seth Frey <moctodliamg@gmail.com>
2018-09-12 17:37:55 +0000

    converted to standalone app
    local ephemeris url, (whole app must have php enabled now)
    accomodating qurick of binary by fixing regex
    cleaned up console logs

--- full.js
+++ full.js
@@ -50,6 +50,11 @@ Number.prototype.mod = function(n) {
  **
 \** * * * * * * * * * * **/
 
+/** * * * * * * * * * * **\
+ ** DEFINE globals
+\** * * * * * * * * * * **/
+const ephemerisURL = 'vendor/swisseph/swetest.php';
+//const ephemerisURL = 'https://enfascination.com/swissephemeris/swetest.php';
 
 /** * * * * * * * * * * **\
  ** DEFINE helpers: generic
@@ -679,7 +684,7 @@ function fetchSwissAstroSun( el ) {
     sdate = omoment.format('D.M.YYYY');
 
         //https://devhints.io/moment
-    console.log(omoment.format(), omoment.utc().format(), omoment.format('D.M.YYYY'), omoment.utc().format('HH:mm:00'));
+    //console.log(omoment.format(), omoment.utc().format(), omoment.format('D.M.YYYY'), omoment.utc().format('HH:mm:00'));
 
     var iday = +omoment.format('D');
     var imonth = +omoment.format('M');
@@ -688,7 +693,7 @@ function fetchSwissAstroSun( el ) {
     var signDiff = (inDesiredSunSign - +currentSunSign ).mod( 12 );
 
     // assemble and submit request to cancel current sign, and define code that handles response text.
-    var astroUrlUndoSun = "https://enfascination.com/swissephemeris/swetest.php?" +
+    var astroUrlUndoSun = ephemerisURL + "?" +
         "b="+ encodeURIComponent(sdate) +
         "&p=0" + "&emos" + "&f=PjLBRsGg" +
         "&house=" + encodeURIComponent(slon) + "%2C" +
@@ -728,7 +733,7 @@ function fetchSwissAstroSun( el ) {
         //console.log(inDesiredSunSign, omoment, "ASDF",desiredMoment, "ASDF", sDesiredMoment, signDiff);
 
         // assemble and submit request to simulate other sign, and define code that handles response text.
-        var astroUrlNewSun = "https://enfascination.com/swissephemeris/swetest.php?" +
+        var astroUrlNewSun = ephemerisURL + "?" +
             "b="+ encodeURIComponent(sDesiredMoment) +
             "&p=0" + "&emos" + "&f=PjLBRsGg" +
             "&house=" + encodeURIComponent(slon) + "%2C" +
@@ -855,13 +860,14 @@ function getAngles(sAstroShell) {
     var sunAngle, sunSign, meridian, ascendant;
     // now use regexes to search the printout for the right digits.
     //  according to the math team, we need angles for the sun, AC and MC.
+    //console.log(sAstroShell);
     sunAngle = sAstroShell.match(
         /\bSun\s+([0-9\.]+)\s+(\d{1,3})°\s?(\d\d?)'\s?(\d\d?).(\d{4})\s+/
     );
     sunSign = Math.floor(+(sunAngle[1]));
     sunAngle = sunAngle[2];
-    ascendant = sAstroShell.match(/\bAscendant\s+(\d{1,3})°\s?(\d\d?)'\s?(\d\d?).(\d{4})\s+/)[1];
-    meridian = sAstroShell.match(/\bMC\s+(\d{1,3})°\s?(\d\d?)'\s?(\d\d?).(\d{4})\s+/)[1];
+    ascendant = sAstroShell.match(/\bAscendant\s+[0-9\.]+\s+(\d{1,3})°\s?(\d\d?)'\s?(\d\d?).(\d{4})\s+/)[1];
+    meridian = sAstroShell.match(/\bMC\s+[0-9\.]+\s+(\d{1,3})°\s?(\d\d?)'\s?(\d\d?).(\d{4})\s+/)[1];
     // convert to radians
     sunAngle = (sunAngle / 360) * 2 * Math.PI;
     ascendant = (ascendant / 360) * 2 * Math.PI;
